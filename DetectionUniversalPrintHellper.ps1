4ba6655a0875e663db19aff677f44584e88dbe51
Jay Williams <jaywilliams916@gmail.com>
2021-09-08 14:52:20 -0700

    Update DetectionUniversalPrintHellper.ps1
    Try/catch doesn't work for non-error generating try statements. Switched to an if/else.

--- DetectionUniversalPrintHellper.ps1
+++ DetectionUniversalPrintHellper.ps1
@@ -1,14 +1,10 @@
 $printerCsv = get-item $env:APPDATA\UniversalPrintPrinterProvisioning\Configuration\printers.csv
 $ts = New-TimeSpan -Days 1
 
-try {
-    if (($printerCsv.LastWriteTime + $ts) -gt (get-date)) {
-        Write-Host "Success"
-        exit 0
-    }
-}
-catch {
-    $errMsg = $_.Exception.Message
-    Write-Host $errMsg
+if (($printerCsv.LastWriteTime + $ts) -gt (get-date)) {
+    Write-Host "Success"
+    exit 0
+} else {
+    Write-Host $printerCsv.Name "needs updated."
     exit 1
 }
